c19986313a39a758b7c163ceefe5d3d363427b1d
tan-map <47981919+tan-map@users.noreply.github.com>
2019-03-03 16:32:36 +0700

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-example_list = [1,"2",30000,"hello",-1,[9999]]
-
-example_list[0] = 2
-if example_list[0]==1:
-    print("First element equal 1")
-else:
-    print("First element not equal 1")
-    print(example_list[0])
